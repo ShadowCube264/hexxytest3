6add9c7116f5e2ee3784ca6fca8baec6bd0edf6a
Wireless <dog783@gmail.com>
2026-08-27 21:54:44 -0400

    added wheelchairs and changed copper support wedge crafting recipe to not be blank

--- kubejs/server_scripts/factory_fix.js
+++ kubejs/server_scripts/factory_fix.js
@@ -1,6 +1,12 @@
 ServerEvents.recipes(e => {
     Ingredient.of("#factory_blocks:factory").itemIds.forEach(id => {
         e.stonecutting(id, "#factory_blocks:factory")
-    })
+    }),
+    event.shapeless(
+        Item.of('createdeco:copper_support_wedge', 3), // arg 1: output
+                    [
+                        'minecraft:copper_ingot'
+                    ]
+    )
 })
 
